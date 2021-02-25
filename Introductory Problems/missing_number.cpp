69ea2ae66aca4ba4701ff9ad7834640c4a5a3e1c
Saikat-98 <saikatchoudhury1998@gmail.com>
2021-02-26 01:03:00 +0530

    [ADDED] Weird Algorithm + Missing Number

---
+++ Introductory Problems/missing_number.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<long long>
+#define eb(x) emplace_back(x)
+#define setbits(x) __builtin_popcountll(x)
+#define tz(x) __builtin_ctzll(x)
+#define lz(x) __builtin_clzll(x)
+#define fsetbit(x) ffsll(x)
+#define mod 1000000007
+#define inf 1e18
+#define deb(x) cout << #x << " " << x << endl;
+#define fo(i, k, n) for (int i = k; i < n; i++)
+#define w(x)  \
+    int x;    \
+    cin >> x; \
+    while (x--)
+#define debug(x, val) cout << "The value of " << x << " is: " << val << "\n";
+#define op(x) cout << x << "\n";
+#define all(x) x.begin(), x.end()
+
+template <typename... T>
+void read(T &...args)
+{
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(T &&...args)
+{
+    ((cout << args << " "), ...);
+}
+
+void fastIO()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void solve()
+{
+    int n, sum = 0;
+    read(n);
+    fo(i, 1, n)
+    {
+        int val;
+        read(val);
+        sum += val;
+    }
+    write((n * (n + 1) / 2) - sum);
+}
+
+int32_t main()
+{
+    fastIO();
+    //   w(t)
+    solve();
+
+    return 0;
+}
+
+// Template designed by Saikat
